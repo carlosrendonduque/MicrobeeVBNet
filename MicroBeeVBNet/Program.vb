4d11ee59ca03072e6da2c9d5c94d03e73c8cccf2
carlosrendonduque <carlos@zen-it.solutions>
2020-11-09 12:26:26 +1000

    Leo first version

--- MicroBeeVBNet/Program.vb
+++ MicroBeeVBNet/Program.vb
@@ -1,4 +1,8 @@
 Imports System
+Imports System.Net.Http
+Imports System.Runtime.Remoting.Messaging
+Imports Newtonsoft.Json.Linq
+Imports Newtonsoft.Json
 
 Module Program
 
@@ -12,107 +16,182 @@ Module Program
     Dim LOGMESSAGE_TRANSITION_SUCCESS As String = "Success"
     Dim LOGMESSAGE_TRANSITION_NO_DATA As String = "No Data"
     Dim MICROBOT_ENVIRONMENT As String = "production"
+    Dim LOGMESSAGE_AUTOMATION_STARTED As String = "Automation Started"
+    Dim ORCHESTRATOR_QUEUE_NAME As String = "orchestrator_queue_name"
+    Dim QUEUE_RABBIT_PUBLISHER As String = "queue_test_microbot"
+    Dim QUEUE_RABBIT_CONSUMER As String = "queue_test_microbot"
+    Dim LOGF_BUSINESS_PROCESS_NAME As String = "framework"
+    Dim MAX_RETRY_NUMBER As Double = 0
+    Dim LOGMESSAGE_GET_TRANSACTION_DATA As String = "Exception calling transaction number"
+    Dim LOGMESSAGE_GET_TRANSACTION_DATA_ERROR As String = "Error getting transaction number"
+    Dim LOGMESSAGE_SUCCESS As String = "Transaction Successful"
+    Dim LOGMESSAGE_BUSINESS_RULE_EXCEPTION As String = "Business Rule Exception"
+    Dim LOGMESSAGE_APPLICATION_EXCEPTION As String = "System Exception"
+    Dim LOGMESSAGE_KILLING_PROCESSES As String = "Killing processes"
+    Dim LOGMESSAGE_OPENING_APPLICATIONS As String = "Opening applications"
+    Dim LOGMESSAGE_CLOSING_APPLICATIONS As String = "Closing applications"
+    Dim LOGMESSAGE_LOADING_TRANSACTION_DATA As String = "Loading transaction data"
+    Dim LOGMESSAGE_TRANSACTION_DATA_LOADED As String = "Loading transaction data"
+    Dim RABBIT_CONNECTION As String = "amqp://localhost"
+    Dim EXTERNAL_SETTINGS_API As String = "http://localhost"
+    Dim EXTERNAL_CONFIG_KEY As String = ""
 
+    Dim JSONString As String
     Dim CONFIG As Dictionary(Of String, String) = New Dictionary(Of String, String)
-
     Dim LOG_PROCESS As Dictionary(Of String, String) = New Dictionary(Of String, String)
-
     Dim TRANSITIONS As Dictionary(Of String, String) = New Dictionary(Of String, String)
-
     Dim STATES As Dictionary(Of String, String) = New Dictionary(Of String, String)
 
+    Dim trm As Task(Of HttpResponseMessage)
+    Dim rm As HttpResponseMessage
+    Dim ts As Task(Of String)
+    ' Create new DataTable instance.
+    Dim table As New DataTable
 
 
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
 
         'initialization
         'Read Configuration file And initialize applications used in the process.
         Dim transition As String
         transition = initialize()
+        If transition = LOGMESSAGE_TRANSITION_SUCCESSFUL Then
+            transition = load_transaction_data()
+            If transition = LOGMESSAGE_TRANSACTION_DATA_LOADED Then
 
+                For Each row As DataRow In table.Rows
+                    Try
+                        save_log("Trace", LOGMESSAGE_TRANSITION_NEW_TRANSACTION)
+                        save_log("Trace", LOGMESSAGE_STATE_TRANSACTION_IN_PROGRESS)
+                        save_log("Trace", "Id: " & row("Id") & " Name: " & row("Name") & " Date: " & row("Date"))
+                        save_log("Trace", LOGMESSAGE_SUCCESS)
+                    Catch ex As Exception
+                        save_log("Trace", LOGMESSAGE_TRANSITION_SYSTEM_EXCEPTION)
+                        'Reintentar MAX_RETRY_NUMBER veces
+                    End Try
 
+                Next row
+            End If
+        Else
+            end_process()
+        End If
+        end_process()
     End Sub
 
+
+    'initialization
+    'Read configuration file And initialize applications used in the process.
     Public Function initialize() As String
         'Try initializing settings And applications
         Try
-
             'Load configurations And open applications
             'If first Then run, read configuration
-            If IsNothing(CONFIG) Then
-
+            If CONFIG.Count = 0 Then
+                initialize_all_settings()
+                kill_all_processes()
             Else
-
             End If
-            LOG_PROCESS.Add("AUTOMATION_STARTED", "")
-            LOG_PROCESS.Add("KILLING_PROCESSES", "")
-            LOG_PROCESS.Add("OPENING_APPLICATIONS", "")
-            LOG_PROCESS.Add("CLOSING_APPLICATIONS", "")
-            LOG_PROCESS.Add("LOADING_TRANSACTION_DATA", "")
-
-
-            TRANSITIONS.Add("SUCCESSFUL", "")
-            TRANSITIONS.Add("NEW_TRANSACTION", "")
-            TRANSITIONS.Add("BUSINESS_EXCEPTION", "")
-            TRANSITIONS.Add("SYSTEM_EXCEPTION", "")
-            TRANSITIONS.Add("SUCCESS", "")
-            TRANSITIONS.Add("NO_DATA", "")
-            TRANSITIONS.Add("LOADING_TRANSACTION_DATA", "")
-
-            STATES.Add("PROCESS_INITIALIZED", "")
-            STATES.Add("DATA_LOADED", "")
-            STATES.Add("TRANSACTION_PROCESSED", "")
-            STATES.Add("PROCESS_FINISHED", "")
-
-            LOG_PROCESS.Add("AUTOMATION_STARTED", LOGMESSAGE_AUTOMATION_STARTED)
-
-            save_log("Trace", LOG_PROCESS.Item("AUTOMATION_STARTED")
-
-            initAllSetObj.initialize_all_settings()
-            KillAllProObj.kill_all_processes()
-
-            Console.WriteLine("Hello World2!")
-            Return ""
+            init_all_applications()
+            initialize = LOGMESSAGE_TRANSITION_SUCCESSFUL
         Catch ex As Exception
-
+            initialize = LOGMESSAGE_APPLICATION_EXCEPTION
         End Try
-
     End Function
 
     Public Sub save_log(ByVal LogLevel As String, ByVal Message As String)
         Console.WriteLine("LogLevel: " & LogLevel & " / " & "Message: " & Message)
     End Sub
 
+    'Initialize, populate And output the configuration values to be used throughout the project.
     Public Sub initialize_all_settings()
-        Global.CONFIG['QUEUE_RABBIT_PUBLISHER'] = process.env.QUEUE_RABBIT_PUBLISHER;
-        Global.CONFIG['QUEUE_RABBIT_CONSUMER'] = process.env.QUEUE_RABBIT_CONSUMER;
-        Global.CONFIG['LOGF_BUSINESS_PROCESS_NAME'] = process.env.LOGF_BUSINESS_PROCESS_NAME;
-        Global.CONFIG['MAX_RETRY_NUMBER'] = process.env.MAX_RETRY_NUMBER;
-        Global.CONFIG['LOGMESSAGE_GET_TRANSACTION_DATA'] = process.env.LOGMESSAGE_GET_TRANSACTION_DATA;
-        Global.CONFIG['LOGMESSAGE_GET_TRANSACTION_DATA_ERROR'] = process.env.LOGMESSAGE_GET_TRANSACTION_DATA_ERROR;
-        Global.CONFIG['LOGMESSAGE_SUCCESS'] = process.env.LOGMESSAGE_SUCCESS;
-        Global.CONFIG['LOGMESSAGE_BUSINESS_RULE_EXCEPTION'] = process.env.LOGMESSAGE_BUSINESS_RULE_EXCEPTION;
-        Global.CONFIG['LOGMESSAGE_APPLICATION_EXCEPTION'] = process.env.LOGMESSAGE_APPLICATION_EXCEPTION;
-        Global.CONFIG['RABBIT_CONNECTION'] = process.env.RABBIT_CONNECTION;
-        Global.CONFIG['QUEUE_RABBIT_PUBLISHER'] = process.env.QUEUE_RABBIT_PUBLISHER;
-        Global.CONFIG['ORCHESTRATOR_QUEUE_NAME'] = process.env.ORCHESTRATOR_QUEUE_NAME;
-        Global.CONFIG['EXTERNAL_SETTINGS_API'] = process.env.EXTERNAL_SETTINGS_API;
-        CONFIG.Add("QUEUE_RABBIT_PUBLISHER", "")
-        CONFIG.Add("QUEUE_RABBIT_CONSUMER", "")
-        CONFIG.Add("LOGF_BUSINESS_PROCESS_NAME", "")
-        CONFIG.Add("MAX_RETRY_NUMBER", "")
-        CONFIG.Add("LOGMESSAGE_GET_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
-        CONFIG.Add("LOADING_TRANSACTION_DATA", "")
+
+        'CONFIG.Add("QUEUE_RABBIT_PUBLISHER", QUEUE_RABBIT_PUBLISHER)
+        'CONFIG.Add("QUEUE_RABBIT_CONSUMER", QUEUE_RABBIT_CONSUMER)
+        'CONFIG.Add("LOGF_BUSINESS_PROCESS_NAME", LOGF_BUSINESS_PROCESS_NAME)
+        'CONFIG.Add("MAX_RETRY_NUMBER", MAX_RETRY_NUMBER)
+        'CONFIG.Add("LOGMESSAGE_GET_TRANSACTION_DATA", LOGMESSAGE_GET_TRANSACTION_DATA)
+        'CONFIG.Add("LOGMESSAGE_GET_TRANSACTION_DATA_ERROR", LOGMESSAGE_GET_TRANSACTION_DATA_ERROR)
+        'CONFIG.Add("LOGMESSAGE_SUCCESS", LOGMESSAGE_SUCCESS)
+        'CONFIG.Add("LOGMESSAGE_BUSINESS_RULE_EXCEPTION", LOGMESSAGE_BUSINESS_RULE_EXCEPTION)
+        'CONFIG.Add("LOGMESSAGE_APPLICATION_EXCEPTION", LOGMESSAGE_APPLICATION_EXCEPTION)
+        'CONFIG.Add("RABBIT_CONNECTION", RABBIT_CONNECTION)
+        'CONFIG.Add("QUEUE_RABBIT_PUBLISHER", QUEUE_RABBIT_PUBLISHER)
+        'CONFIG.Add("ORCHESTRATOR_QUEUE_NAME", ORCHESTRATOR_QUEUE_NAME)
+        'CONFIG.Add("EXTERNAL_SETTINGS_API", EXTERNAL_SETTINGS_API)
+        'CONFIG.Add("EXTERNAL_CONFIG_KEY", EXTERNAL_CONFIG_KEY)
+
+        save_log("Trace", LOGMESSAGE_AUTOMATION_STARTED)
 
     End Sub
+
+
+    'Use the Kill Process activity to force the termination of  processes representing applications used in the business process being automated.
+    'Note that killing processes might have undesirable outcomes, such as losing unsaved changes to files.    
+    Public Sub kill_all_processes()
+
+        'TODO Code to force the termination of  processes representing applications used in the business process being automated
+        save_log("Trace", LOGMESSAGE_KILLING_PROCESSES)
+
+    End Sub
+
+    'Open applications used in the process and do necessary initialization procedures (e.g., login).
+    Public Sub init_all_applications()
+
+        'TODO: Code to open applications used in the business process being automated
+        save_log("Trace", LOGMESSAGE_OPENING_APPLICATIONS)
+
+    End Sub
+
+
+
+    'Do the necessary procedures for ending the process (e.g., logout) And close the used applications.
+    Public Sub close_all_applications()
+        Try
+            'TODO: Code to open applications used in the business process being automated
+            save_log("Trace", LOGMESSAGE_CLOSING_APPLICATIONS)
+        Catch ex As Exception
+            kill_all_processes()
+        End Try
+
+    End Sub
+
+    'Do the necessary procedures for ending the process (e.g., logout) And close the used applications.
+    Public Sub end_process()
+        close_all_applications()
+        'TODO: Code to open applications used in the business process being automated
+        save_log("Trace", LOGMESSAGE_STATE_PROCESS_FINISHED)
+
+    End Sub
+    Function GetTable() As DataTable
+
+        Return table
+    End Function
+
+    'Get all transaction items from a specified source (e.g., queues, spreadsheets, databases, mailboxes, web APIs Or any other)
+    Public Function load_transaction_data() As String
+
+
+        Try
+
+            save_log("Trace", LOGMESSAGE_LOADING_TRANSACTION_DATA)
+
+
+            ' Create four typed columns in the DataTable.
+            table.Columns.Add("Id", GetType(Integer))
+            table.Columns.Add("Name", GetType(String))
+            table.Columns.Add("Date", GetType(DateTime))
+
+            ' Add five rows with those columns filled in the DataTable.
+            table.Rows.Add(25, "Juan", DateTime.Now)
+            table.Rows.Add(50, "David", DateTime.Now)
+            table.Rows.Add(10, "Christoff", DateTime.Now)
+            table.Rows.Add(21, "Janet", DateTime.Now)
+            table.Rows.Add(100, "Melanie", DateTime.Now)
+
+            load_transaction_data = LOGMESSAGE_TRANSACTION_DATA_LOADED
+        Catch ex As Exception
+            '...handle exception...
+            load_transaction_data = LOGMESSAGE_APPLICATION_EXCEPTION
+        End Try
+    End Function
 End Module
